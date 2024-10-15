a7acb9b6a0a1fcb6911f431dc5c49934c0f9f114
Tanishq Trivedi <tanishq.trivedi27@gmail.com>
2024-10-15 20:32:02 +0530

    initial commit

---
+++ src/test/java/github/tanishqtrivedi27/expenseService/ExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.tanishqtrivedi27.expenseService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
